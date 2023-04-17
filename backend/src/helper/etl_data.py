3926d377d2b1ea9d152b0efde962372a0c2da2bd
Calvin Du <calvingdu@gmail.com>
2023-04-16 22:13:54 -0400

    mongo

--- backend/src/helper/etl_data.py
+++ backend/src/helper/etl_data.py
@@ -1,17 +1,17 @@
+import pandas as pd 
 from backend.src.helper.iterate_datasets import move_file
 from backend.src.helper.iterate_datasets import change_transaction_file_name
-import pandas as pd 
+from backend.src.mongoDB.mongodb_hook import insert_df
+from backend.src.mongoDB.mongodb_hook import delete_df
 
 def load_dataset(file_name, file_path, account_name, df):
         file_name = change_transaction_file_name(file_path, file_name, account_name, df)
         now_date = pd.Timestamp.now() 
-        df['processed_date'] = now_date
-        print(df)
-
-        #df.to_csv(file_path+account_name+'.csv')
+        df['processed_date'] = now_date 
         
-        target_directory = 'processed/'+account_name+'/'
-        #move_file(file_name, file_path, target_directory)
+        insert_df(df)
+        target_directory = 'processed/'
+        move_file(file_name, file_path, target_directory)
 
 def common_transform(df, account_name):
         df.columns = df.columns.map(lambda x: x.lower())
