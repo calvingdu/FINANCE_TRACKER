44a2dbeeac04850daa091ca6d05864fcb716fb90
Calvin Du <calvingdu@gmail.com>
2023-07-16 16:08:25 -0400

    config

--- configuration/config_setup.py
+++ configuration/config_setup.py
@@ -2,16 +2,20 @@ from __future__ import annotations
 
 import os
 import sys
+from pathlib import Path
 
 from dotenv import load_dotenv
 
 sys.path.insert(0, ".")
 
+configuration_directory = Path(__file__).parents[0]
+
 if os.environ.get("PYTHON_ENV"):
+    print(configuration_directory)
     print(f"{os.environ.get('PYTHON_ENV')}.env")
-    load_dotenv(f"config/{os.environ.get('PYTHON_ENV')}.env")
+    load_dotenv(f"{configuration_directory}/{os.environ.get('PYTHON_ENV')}.env")
 else:
     print("Loading local.env")
-    load_dotenv("config/local.env")
+    load_dotenv(f"{configuration_directory}/local.env")
 
 config = os.environ.copy()
