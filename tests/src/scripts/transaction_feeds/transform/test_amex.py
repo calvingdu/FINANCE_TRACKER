2d9b60df3d5921179d14c6da1546f42b8c714659
Calvin Du <calvingdu@gmail.com>
2023-07-16 16:16:21 -0400

    test

--- tests/src/scripts/transaction_feeds/transform/test_amex.py
+++ tests/src/scripts/transaction_feeds/transform/test_amex.py
@@ -3,7 +3,6 @@ from __future__ import annotations
 import pandas as pd
 from pandas import Timestamp
 
-from configuration.config_setup import config
 from src.scripts.transaction_feeds.accounts_config import accounts_config
 from src.scripts.transaction_feeds.transform.transform_amex_transactions import (
     transform_dataset,
